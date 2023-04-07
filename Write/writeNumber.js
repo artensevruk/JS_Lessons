f75d658c3618a45a94c84c34da772ab887fc9b9f
Artem Sevruk <96494811+artensevruk@users.noreply.github.com>
2023-04-07 14:27:03 +0300

    write

---
+++ Write/writeNumber.js
@@ -0,0 +1,15 @@
+// const a = prompt("Write Number");
+// window.alert(a);
+
+const readNumber = () => {
+    let numberN ;
+    do {
+        numberN = prompt ("Namber");
+    } while ( numberN != 2 )
+    alert ("Ура");
+}
+
+readNumber()
+
+
+
