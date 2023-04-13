3861b1b5becaf52badce10417e451160fe7b11f4
Artem Sevruk <96494811+artensevruk@users.noreply.github.com>
2023-04-13 15:11:17 +0300

    Artem

--- Line_segment.js
+++ Line_segment.js
@@ -12,19 +12,18 @@ const  LineSegment =(x1 ,y1 ,x2 ,y2) =>{
         console.log("отрезки пересекаются");
         return
     } 
+    if(x1 > x2 && y1 < y2){
+        console.log("отрезки пересекаются");
+        return
+    }
     if(x1 < x2 && y1 > y2){
         console.log("отрезки пересекаются");
         return
     }
+    
     console.log("Отрезки не пересекаются");
 }
 LineSegment(4 , 5 , 1 ,6);
 LineSegment(3 , 3 , 4, 6);
-LineSegment(2 , 3 , 1, 5);
+LineSegment(1, 5, 3, 6);
 
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https://github.com/artensevruk/line.git
-// git push -u origin main
