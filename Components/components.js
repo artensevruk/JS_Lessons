0a13278fc15c0e929824e6cea0283b7a57c07fb7
Artem Sevruk <96494811+artensevruk@users.noreply.github.com>
2023-04-19 16:19:13 +0300

    counterComponent

--- Components/components.js
+++ Components/components.js
@@ -1,45 +1,55 @@
-
-
 const list = (items) => {
-  
   const list = document.createElement("ul");
- 
-//   for(let i = 0 ; i < items.length ; i++){
-//     const listElement = document.createElement("li");
-//     list.append(listElement);
-//     listElement.innerText = items[i];
-//   }
 
-// items.forEach(element => {
-//     const listElement = document.createElement("li");
-//     list.append(listElement);
-//     listElement.innerText = element;
-// });
+  //   for(let i = 0 ; i < items.length ; i++){
+  //     const listElement = document.createElement("li");
+  //     list.append(listElement);
+  //     listElement.innerText = items[i];
+  //   }
 
-for(const i of items){
+  // items.forEach(element => {
+  //     const listElement = document.createElement("li");
+  //     list.append(listElement);
+  //     listElement.innerText = element;
+  // });
+
+  for (const i of items) {
     const listElement = document.createElement("li");
     list.append(listElement);
     listElement.innerText = i;
-}
-
+  }
 
   return list;
 };
 
-
-const headerList = (title , items) =>{
+const headerList = (title, items) => {
   const header = document.createElement("h1");
-  header.innerText = title ;
-  const wrapper =document.createElement("div");
- 
-  const listComponent = list(items) ;
-  wrapper.append(header , listComponent);
-  return wrapper ;
-}
+  header.innerText = title;
+  const wrapper = document.createElement("div");
 
+  const listComponent = list(items);
+  wrapper.append(header, listComponent);
+  return wrapper;
+};
 
+const counter = (count = 0) => {
+  const container = document.createElement("div");
+  const buttonPlus = document.createElement("button");
+  const buttonMinus = document.createElement("button");
+  const countSpan = document.createElement("span");
+  buttonMinus.innerText = "-";
+  buttonPlus.innerText = "+";
+  countSpan.innerText = count;
+  container.append(buttonMinus, countSpan, buttonPlus);
 
+  const getHandler = delta => () => {
+    count += delta;
+    countSpan.innerText = count;
+  };
 
+  buttonPlus.addEventListener("click", getHandler(1));
 
+  buttonMinus.addEventListener("click", getHandler(-1));
 
-
+  return container;
+};
--- Components/index.js
+++ Components/index.js
@@ -6,8 +6,16 @@ const fruits = ["apple", "banana" , "Cherry"];
 document.body.append(
 list(colors),
 list(fruits),
-headerList("numbers" , [1 , 2 , 2])
+headerList("numbers" , [1 , 2 , 2]),
+
+//----------------------------------
+
+counter(2),
+counter()
+
 );
+
+
 
 
 
