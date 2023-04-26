1a429c5220d3ba05493d885ec1134d982f5efebc
Anton <kurguzenkov@gmail.com>
2023-04-26 23:02:42 +0300

    objects tasks

---
+++ Objects/functions.js
@@ -0,0 +1,15 @@
+/*
+ * Получение массива всех имен сотрудников
+ */
+
+export const getNames = () => {}; // employees => arrayOfNames
+
+/*
+ * Подсчет суммы зарплат сотрудников
+ */
+export const getSalariesSum = () => {}; // employees => salariesSum
+
+/*
+ * Подсчет общей стоимости корзины
+ */
+export const getCartTotalPrice = () => {}; // (prices, cart) => cartTotalPrice
