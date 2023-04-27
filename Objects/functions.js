46797a810ef754a14dfa28b1cceba2b6dab6ddf9
Artem Sevruk <96494811+artensevruk@users.noreply.github.com>
2023-04-27 22:09:17 +0300

    functions

--- Objects/functions.js
+++ Objects/functions.js
@@ -2,7 +2,19 @@
  * Получение массива всех имен сотрудников
  */
 
-export const getNames = () => {}; // employees => arrayOfNames
+export const getNames = (employees) => {
+  const array = [];
+  for (let i = 0; employees.length > i; i++) {
+    const employee = employees[i];
+    console.log(employee.name);
+    array.push(employee.name);
+  }
+  return array;
+}; // employees => arrayOfNames
+
+export const sum = (a, b) => {
+  return a + b;
+};
 
 /*
  * Подсчет суммы зарплат сотрудников
