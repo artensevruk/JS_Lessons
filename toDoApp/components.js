3de1ac2d15fcf3555c856a810408f2daa54c43aa
Artem Sevruk <96494811+artensevruk@users.noreply.github.com>
2023-05-09 13:14:21 +0300

    ToDo

--- toDoApp/components.js
+++ toDoApp/components.js
@@ -1,20 +1,24 @@
 import { createElement } from "../domUtils.js";
 
 export const inlineForm = (onsubmit) => {
-    const input = createElement("input");
-    const button = createElement("input", { type: "submit" });
-    const inlineForm = createElement("form", [input, button]);
-  
-    inlineForm.onsubmit = (event) => {
-      onsubmit(input.value);
-      event.preventDefault();
-      input.value = "";
-    };
-  
-    return inlineForm;
+  const input = createElement("input");
+  const button = createElement("input", { type: "submit" });
+  const inlineForm = createElement("form", [input, button]);
+
+  inlineForm.onsubmit = (event) => {
+    onsubmit(input.value);
+    event.preventDefault();
+    input.value = "";
   };
-  
-  
-  export const todoList = () => {
-    return createElement("div");
-  };
+
+  return inlineForm;
+};
+
+export const todoList = (elements) => {
+  const childElements = elements.map((element) =>
+    createElement("div", element)
+  );
+  const list = createElement("div", childElements);
+
+  return list;
+};
--- toDoApp/toDo.js
+++ toDoApp/toDo.js
@@ -1,17 +1,21 @@
 import { inlineForm  , todoList} from "./components.js";
 import{createElement} from "../domUtils.js"
 
+
 export const toDoApp = () => {
+  let array  = JSON.parse(localStorage.getItem("to-do")) || [] ;
+ 
+   
+  
     const form = inlineForm((text) => {
       const div = createElement("div", text);
-  
-      let array = [];
-      localStorage.setItem(text, JSON.stringify(text));
-      array = JSON.parse(localStorage.getItem(text));
-  
+      array.push(text);
+     localStorage.setItem("to-do" , JSON.stringify(array)); 
+
       list.append(div);
     });
-    const list = todoList();
+    const list = todoList(array);
+    
     const div = createElement("div", [form, list]);
     return div;
 }
