9ae5e912fe9c6b289fed344dfc0f64ea3d528818
Artem <sevruk04@bk.ru>
2023-12-14 22:26:16 +0300

    codeImprovement

---
+++ loadingImg/imageSearch.js
@@ -0,0 +1,12 @@
+const getImage = (imageUrl) => {
+  const image = document.createElement("img");
+
+  return new Promise((resolve) => {
+    image.onload = () => {
+      resolve(image);
+    };
+    image.src = imageUrl;
+  });
+};
+
+export { getImageSize };
--- loadingImg/index.js
+++ loadingImg/index.js
@@ -1,7 +1,7 @@
 
-import {getImageSize} from './imageSearch.js';
+import {getImage} from './imageSearch.js';
 
 
-getImageSize(
+getImage(
   "https://static.boredpanda.com/blog/wp-content/uploads/2015/07/smiling-cat-33__605.jpg"
 ).then((image) => console.log([image.width, image.height]));
