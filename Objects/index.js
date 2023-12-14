16b2798ca16d26c0c16003282189b0a7e5fc49ac
Artem Sevruk <96494811+artensevruk@users.noreply.github.com>
2023-12-14 19:20:29 +0300

    functions

--- Objects/index.js
+++ Objects/index.js
@@ -1,3 +1,6 @@
 /*
  * Тут можно импортировать и вызывать функции, логировать результат их выполнения
  */
+import { getNames, sum } from "./functions.js";
+console.log(sum(6 ,7));
+console.log(getNames([{ name: 'John Doe' } , {name: 'Jane Doe'} ]));
